61c0c9d28683917e4b9f654ecc4bce115c8ffba1
Ruben LZ Tan <soggie@gmail.com>
2011-08-06 13:12:57 +0800

    Dropbox oauth

--- main.js
+++ main.js
@@ -2,6 +2,16 @@ var dropbox = require('dropbox');
 var express = require('express');
 var everyauth = require('everyauth');
 var ejs = require('ejs');
+var fs = require('fs');
+
+everyauth.dropbox
+    .entryPath('/login')
+    .consumerKey('hm6mivak4p6iwfs')
+    .consumerSecret('5z3mljqqlw9apue')
+    .findOrCreateUser( function(sess, accessToken, accessSecret, user) {
+        console.log('identified user');
+    })
+    .redirectPath('/');
 
 var server = express.createServer();
 server.configure(function() {
@@ -9,14 +19,21 @@ server.configure(function() {
     server.set('view engine', 'html');
     server.set('views', __dirname + '/view');
 
+    server.use(express.bodyParser());
     server.use(express.cookieParser());
     server.use(express.session({ secret : 'We are dropbox people' }));
     server.use(express.static(__dirname + '/public'));
+    server.use(everyauth.middleware());
+    server.use(server.router);
+
+    everyauth.helpExpress(server);
 });
 
 server.get('/', function(req, res){
     res.render('index.html');
 });
+
+var users = { 'foo' : 'bar' };
 
 server.listen(1337);
 
